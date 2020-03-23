3a896cf073ac9f8163465d4d2496a2531f8722f0
Danila Kostenko <danil.kostenko@outlook.com>
2020-03-23 22:17:56 +0300

    add web api controller for ajax request

---
+++ WishList/WishList/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace WishList.Controllers.Api
+{
+    public class ValuesController : ApiController
+    {
+        // GET: api/Profile
+        [HttpGet, Route("api/FullWorkflow")]
+        public IHttpActionResult Get()
+        {
+            var arr = new string[] { "value1", "value2" };
+            var s = JsonConvert.SerializeObject(arr);
+            return Ok(s);
+        }
+
+        // GET: api/Profile/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Profile
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Profile/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Profile/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- WishList/WishList/Global.asax.cs
+++ WishList/WishList/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -17,6 +18,7 @@ namespace WishList
             AutofacConfig.Initialize();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
