7c329e1bb1d2edb399c844657995ad2b41fa6f1e
Danila Kostenko <danil.kostenko@outlook.com>
2020-04-05 15:14:51 +0300

    add search users function

--- WishList/WishList/Scripts/Pages/SearchUsers.js
+++ WishList/WishList/Scripts/Pages/SearchUsers.js
@@ -6,7 +6,6 @@
         const params = new URLSearchParams({
             term: input.value,
         });
-        console.log(`https://localhost:44360/api/FindUser/?${params.toString()}`);
         fetch(`https://localhost:44360/api/FindUsers?term=${input.value}`)
             .then(res => res.json())
             .then(res => {
@@ -18,18 +17,14 @@
                     container.innerHTML = '';
                     const fragment = document.createDocumentFragment();
                     const data = JSON.parse(res);
-                    console.log(data);
                     data.forEach(ob => {
                         const linkContainer = document.createElement('div');
-                        linkContainer.classList.add('mb-10');
                         const link = document.createElement('a');
-                        link.style.color = 'white';
                         link.innerHTML = `${ob.FullName} (${ob.Email})`;
                         link.href = `https://localhost:44360/Profile/AnotherUser/${ob.Id}`;
                         linkContainer.appendChild(link);
                         fragment.appendChild(linkContainer);
                     })
-                    console.log(container);
                     container.appendChild(fragment);
                 }
             })
